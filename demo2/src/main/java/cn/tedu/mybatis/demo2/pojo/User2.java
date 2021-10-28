3f6af1ed3468f4e99a778bc00022db9259354c41
Loovemaker <bows-02.roper@icloud.com>
2021-10-28 17:23:16 +0800

    init 2
    // TODO: Find out WHY ALIAS functionalitiy in the fuxxin xml cunt FULLY work wotule

--- demo2/src/main/java/cn/tedu/mybatis/demo2/pojo/User2.java
+++ demo2/src/main/java/cn/tedu/mybatis/demo2/pojo/User2.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.apache.ibatis.type.Alias;
+
+import java.io.Serializable;
 
 @Data
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class User2 {
-
+public class User2 implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String sex;
 }
