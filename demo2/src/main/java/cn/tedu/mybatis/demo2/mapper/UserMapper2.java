2cd0b3342b416998f2ff73494d76bf248614fe5e
Loovemaker <bows-02.roper@icloud.com>
2021-10-29 14:29:47 +0800

    .

--- demo2/src/main/java/cn/tedu/mybatis/demo2/mapper/UserMapper2.java
+++ demo2/src/main/java/cn/tedu/mybatis/demo2/mapper/UserMapper2.java
@@ -2,10 +2,21 @@ package cn.tedu.mybatis.demo2.mapper;
 
 import cn.tedu.mybatis.demo2.pojo.User2;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper2 {
     List<User2> listAllUsers();
+    List<User2> findIn(int[] ids);
+    List<User2> findInList(List<Integer> ids);
+    List<User2> findInMap(
+            @Param("ids") int[] ids,
+            @Param("sex") String sex
+    );
+
+    List<User2> findSqlWhere(User2 user2);
+
+    Integer sqlUpdate(User2 user2);
 }
--- demo2/src/test/java/cn/tedu/mybatis/demo2/TestMybatis.java
+++ demo2/src/test/java/cn/tedu/mybatis/demo2/TestMybatis.java
@@ -1,6 +1,7 @@
 package cn.tedu.mybatis.demo2;
 
 import cn.tedu.mybatis.demo2.mapper.UserMapper2;
+import cn.tedu.mybatis.demo2.pojo.User2;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -8,10 +9,18 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class TestMybatis {
 
     String resourcePath = "mybatis/mybatis-config.xml";
+
+    Random random = new Random();
+
+    String randomGender() {
+        return random.nextBoolean() ? "男" : "女";
+    }
 
     SqlSession getSession(String resourcePath) {
         SqlSession result;
@@ -35,6 +44,49 @@ public class TestMybatis {
                     .getMapper(UserMapper2.class)
                     .listAllUsers()
                     .forEach(System.out::println);
+        }
+    }
+
+    @Test void findInTest() {
+        final int[] ids = IntStream.range(0, 100).toArray();
+
+        try (final SqlSession sqlSession = getSession(resourcePath)) {
+            sqlSession
+                    .getMapper(UserMapper2.class)
+                    .findIn(ids)
+                    .forEach(System.out::println);
+        }
+    }
+
+    @Test void findInMapTest() {
+        final int[] ids = IntStream.range(0, 100).toArray();
+        String male = "男";
+
+        try (final SqlSession sqlSession = getSession(resourcePath)) {
+            sqlSession
+                    .getMapper(UserMapper2.class)
+                    .findInMap(ids, male)
+                    .forEach(System.out::println);
+        }
+    }
+
+    @Test void findSqlWhereTest() {
+        final User2 user2 = new User2(null, "金角大王", 3000, "男");
+        try (final SqlSession sqlSession = getSession(resourcePath)) {
+            sqlSession
+                    .getMapper(UserMapper2.class)
+                    .findSqlWhere(user2)
+                    .forEach(System.out::println);
+        }
+    }
+
+    @Test void sqlUpdateTest() {
+        final User2 守山使者 = new User2(4, "守山使者", 5000, null);
+
+        try (final SqlSession sqlSession = getSession(resourcePath)) {
+            System.out.printf("%d rows affected", sqlSession
+                    .getMapper(UserMapper2.class)
+                    .sqlUpdate(守山使者));
         }
     }
 }
