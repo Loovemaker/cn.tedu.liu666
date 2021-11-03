b36b4972f6ddc98ec223ec41e75bd15f0a2c0460
Loovemaker <bows-02.roper@icloud.com>
2021-11-03 17:30:50 +0800

    前后端交互

--- ssm/src/main/java/cn/tedu/jt/ssm/controller/UserController.java
+++ ssm/src/main/java/cn/tedu/jt/ssm/controller/UserController.java
@@ -4,38 +4,37 @@ package cn.tedu.jt.ssm.controller;
 import cn.tedu.jt.ssm.pojo.User;
 import cn.tedu.jt.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("user")
 public class UserController {
 
     @Autowired private UserService userService;
 
-    @RequestMapping("findall")
+    @GetMapping("findall")
     public String findAll() {
         return userService.findAll().replaceAll("\n", "<br />\n");
     }
 
-    @RequestMapping("find/byid/{id}")
+    @GetMapping("find/byid/{id}")
     public String findUserById(@PathVariable Integer id) {
         return userService.findUserById(id);
     }
 
-    @RequestMapping("find/byids")
+    @GetMapping("find/byids")
     public String findUserByIds(Integer[] ids) {
         return userService.findUserByIds(ids).replaceAll("\n", "<br />\n");
     }
 
-    @RequestMapping("find/byas")
+    @GetMapping("find/byas")
     public String findUserByAS(User user) {
         return userService.findUserByAS(user).replaceAll("\n", "<br />\n");
     }
 
-    @RequestMapping("update/byid/{id}/{name}/{age}/{sex}")
+    @PutMapping(value = "update/byid/{id}/{name}/{age}/{sex}")
     public String updateUserById(User user) {
         return userService.updateUserById(user);
     }
