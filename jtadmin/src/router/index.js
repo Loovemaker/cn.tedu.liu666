2411175ff0d16ed35c0806a593bc9e570796c08d
Loovemaker <bows-02.roper@icloud.com>
2021-11-08 10:55:46 +0800

    路由导航守卫

--- jtadmin/src/router/index.js
+++ jtadmin/src/router/index.js
@@ -2,18 +2,29 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import ElementUI from '../components/ElementUI.vue'
+import Home from '../components/Home.vue'
 //使用路由机制
 Vue.use(VueRouter)
 const routes = [
   {path: '/', redirect: '/login'},
   {path: '/login', component: Login},
-  {path: '/elementUI', component: ElementUI}
+  {path: '/elementUI', component: ElementUI},
+  {path: '/home', component: Home},
 ]
 
 //路由导航守卫!!!!!!!
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.path === '/login') return next()
+
+  let token = window.sessionStorage.getItem('token')
+  if(!token) return next('/login')
+  
+  return next()
 })
 
 export default router
