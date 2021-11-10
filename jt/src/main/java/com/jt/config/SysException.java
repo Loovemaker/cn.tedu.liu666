5493a9032326376d8711480c7cb2431b26611d9c
Loovemaker <bows-02.roper@icloud.com>
2021-11-10 11:55:39 +0800

    introduce global exception handling

---
+++ jt/src/main/java/com/jt/config/SysException.java
@@ -0,0 +1,15 @@
+package com.jt.config;
+
+import com.jt.vo.SysResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class SysException {
+
+    @ExceptionHandler(RuntimeException.class)
+    public SysResult failed(Throwable throwable) {
+        throwable.printStackTrace();
+        return SysResult.failed();
+    }
+}
