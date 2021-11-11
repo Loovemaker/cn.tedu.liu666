b1d8727ce6eb8e8f27e715b01a5cce3ec8568b7a
Loovemaker <bows-02.roper@icloud.com>
2021-11-11 15:17:19 +0800

    optimize SQL(I/O) usage

--- jt/src/main/java/com/jt/service/ItemCatServiceImpl.java
+++ jt/src/main/java/com/jt/service/ItemCatServiceImpl.java
@@ -2,13 +2,15 @@ package com.jt.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.ItemCatMapper;
-import com.jt.misc.MybatisPlusUtils;
 import com.jt.pojo.ItemCat;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
 
 @Service
 public class ItemCatServiceImpl implements ItemCatService{
@@ -18,35 +20,48 @@ public class ItemCatServiceImpl implements ItemCatService{
 
     @Override
     public List<ItemCat> findItemCatList(Integer level) {
-        if (!(level > 0)) return null;
-
-        AtomicReference<List<ItemCat>> itemCatsWithoutChildren = new AtomicReference<>();
-        MybatisPlusUtils.wrapperExecution(queryWrapper, itemCatWrapper ->
-            itemCatsWithoutChildren.set(mapper.selectList(queryWrapper
-                    .eq("level", 1)
-                    .eq("parent_id", 0)
-            ))
-        );
-        return fillItemCatChildren(itemCatsWithoutChildren.get(), level);
+        val ROOT_PARENT = 0;
+        return fillChildren(getAllItemCatMapByParentId(), level)
+                .get(ROOT_PARENT);
     }
 
-    List<ItemCat> fillItemCatChildren(List<ItemCat> target, Integer maxLevel) {
-        target.forEach(item -> {
-            if (!(item.getLevel() < maxLevel)) return;
-
-            AtomicReference<List<ItemCat>> children = new AtomicReference<>();
-            MybatisPlusUtils.wrapperExecution(queryWrapper, itemCatWrapper ->
-                    children.set(mapper.selectList(queryWrapper
-                            .eq("level", item.getLevel() + 1)
-                            .eq("parent_id", item.getId())
-                    ))
-            );
-
-            if (!children.get().isEmpty()) {
-                fillItemCatChildren(children.get(), maxLevel);
-                item.setChildren(children.get());
-            }
+    Map<Integer, List<ItemCat>> getAllItemCatMapByParentId() {
+        val parentIdMap = new HashMap<Integer, List<ItemCat>>();
+        mapper.selectList(null).forEach(itemCat -> {
+            val parentId = itemCat.getParentId();
+            if (!parentIdMap.containsKey(parentId))
+                parentIdMap.put(parentId, new LinkedList<>());
+            parentIdMap.get(parentId).add(itemCat);
         });
+        return parentIdMap;
+    }
+
+    Map<Integer, List<ItemCat>> fillChildren(
+            final Map<Integer, List<ItemCat>> target,
+            Integer maxLevel
+    ) {
+        if (!(maxLevel > 0)) return null;
+
+        val ROOT_PARENT = 0;
+        target.get(ROOT_PARENT).forEach(itemCat ->
+            fillChildren(target, itemCat, maxLevel)
+        );
         return target;
+    }
+
+    void fillChildren(
+            final Map<Integer, List<ItemCat>> target,
+            ItemCat itemCat,
+            Integer maxLevel
+    ) {
+        if (!(itemCat.getLevel() < maxLevel)) return;
+
+        val children = target.get(itemCat.getId());
+        if (!children.isEmpty()) {
+            children.forEach(itemCat1 -> {
+                itemCat.setChildren(children);
+                fillChildren(target, itemCat1, maxLevel);
+            });
+        }
     }
 }
